2706a827151f1d7c22975ce14096f37726bab183
Guy <guythomas721@gmail.com>
2017-03-28 22:37:24 +1100

    Fix bug on transition however notes are not rendered correctly

--- App/Components/Notes.js
+++ App/Components/Notes.js
@@ -1,132 +1,138 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  ListView,
-  TextInput,
-  StyleSheet,
-  TouchableHighlight,
-} from 'react-native';
 import Separator from './Helpers/Separator';
 import Badge from './Badge';
+import React from 'react';
+import {
+	View,
+	Text,
+	ListView,
+	TextInput,
+	StyleSheet,
+	TouchableHighlight
+} from 'react-native';
 
+const api = require("../Utils/api");
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: 'column'
-	},
-	buttonText: {
-		fontSize: 18,
-		color: 'white'
-	},
-	button: {
-		height: 60,
-		backgroundColor: '#48BBEC',
-		flex: 3,
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	searchInput: {
-		height: 60,
-		padding: 10,
-		fontSize: 18,
-		color: '#111',
-		flex: 10
-	},
-	rowContainer: {
-		padding: 10
-	},
-	footerContainer: {
-		backgroundColor: '#E3E3E3',
-		alignItems: 'center',
-		flexDirection: 'row'
-	}
-})
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "white"
+  },
+  button: {
+    height: 60,
+    backgroundColor: "#48BBEC",
+    flexGrow: 3,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  searchInput: {
+    height: 60,
+    padding: 10,
+    fontSize: 18,
+    color: "#111",
+    flex: 10
+  },
+  rowContainer: {
+    padding: 10,
+  },
+  footerContainer: {
+    backgroundColor: "#E3E3E3",
+    alignItems: "center",
+    flexDirection: "row"
+  }
+});
 
 class Notes extends React.Component{
-	constructor(props) {
-		super(props)
-		this.ds = new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2})
-		this.state = {
-			dataSource: this.ds.cloneWithRows(this.props.notes),
-			note: '',
-			error: ''
-		}
-	}
+  constructor(props) {
+    super(props);
 
-	componentDidMount() {
-		console.log('Did mount')
-	}
+    this.ds = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2
+    });
 
-	handleChange(e) {
-		this.setState({
-			note: e.nativeEvent.text
-		})
-	}
+    this.state = {
+      dataSource: this.ds.cloneWithRows(this.props.notes),
+      note: "",
+      error: ""
+    };
+  }
 
-	handleSubmit() {
-		const note = this.state.note;
-		this.setState({
-			note: ''
-		})
+  handleChange(event) {
+    this.setState({
+      note: event.nativeEvent.text
+    });
+  }
 
-		api.addNote(this.props.userInfo.login, note)
-		.then((data) => {
-			this.setState({
-				dataSource: this.ds.cloneWithRows(data)
-			})
-		})
-		.catch((err) => {
-			console.log('Request failed', err)
-			this.setState({err})
-		})
-	}
+  handleSubmit() {
+    var note = this.state.note;
+    
+    this.setState({
+      note: ""
+    });
 
-	footer() {
-		return (
-			<View>
-				<TextInput
-					style={styles.searchInput}
-					value={this.state.note}
-					onChange={this.handleChange.bind(this)}
-					placeholder='New Note' />
-				<TouchableHighlight 
-					style={styles.button}
-					onPress={this.handleSubmit.bind(this)}
-					underlayColor="#88D4F5" >
-						<Text style={styles.buttonText}>Submit</Text>
-				</TouchableHighlight>
-			</View>
-			)
-	}
+    api.addNote(this.props.userinfo.login, note)
+      .then((data) => {
+        api.getNotes(this.props.userinfo.login)
+          .then((notes) => {
+            this.setState({
+              dataSource: this.ds.cloneWithRows(notes)
+            });
+          })
+      })
+      .catch((error) => {
+        console.error(error);
+        this.setState( {error} );
+      });
+  }
 
-	renderRow(rowData) {
-		return (
-			<View>
-				<View style={styles.rowContainer}>
-					<Text>{rowData}</Text>
-				</View>
-				<Separator />
-			</View>
-			)
-	}
+  renderRow(rowData) {
+    return (
+      <View>
+        <View style={styles.rowContainer}>
+          <Text> {rowData} </Text>
+        </View>
+        <Separator />
+      </View>
+    )
+  }
 
-	render() {
-		return (
-			<View style={styles.container}>
-			<ListView
-				dataSource={this.state.dataSource}
-				renderRow={this.renderRow}
-				renderHeader={() => {<Badge userinfo={this.props.userinfo} />}} />
-				{this.footer()}
-			</View>
-		)
-	}
-}
+  footer() {
+    return (
+      <View style={styles.footContainer}>
+        <TextInput
+          style={styles.searchInput}
+          value={this.state.note}
+          onChange={this.handleChange.bind(this)}
+          placeholder="New Note" />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.handleSubmit.bind(this)}
+          underlayColor="#88D4F5">
+
+          <Text style={styles.buttonText}> Submit </Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+          renderHeader={() => <Badge userinfo={this.props.userinfo} />} />
+        {this.footer()}
+      </View>
+    )
+  }
+};
 
 Notes.propTypes = {
-	userinfo: React.PropTypes.object.isRequired,
-	notes: React.PropTypes.object.isRequired
-}
+  userinfo: React.PropTypes.object.isRequired,
+  notes: React.PropTypes.object.isRequired
+};
 
 module.exports = Notes;
