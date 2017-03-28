3ed9f1fdd62b20bad59321198426a765cd819fc7
Guy <guythomas721@gmail.com>
2017-03-28 12:09:32 +1100

    Initialised repo and add home page

---
+++ App/Components/Main.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  PropTypes,
+  TextInput,
+  ActivityIndicatorIOS,
+  TouchableHighlight
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 30,
+    marginTop: 65,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'#48BBEC'
+  },
+  title: {
+  	color: 'white',
+  	fontSize: 25,
+  	// textAlign: 'center'
+  },
+  searchInput: {
+  	height: 50,
+  	padding: 4,
+  	marginRight: 5,
+  	fontSize: 23,
+  	borderWidth: 1,
+  	borderColor: 'white',
+  	borderRadius: 8,
+  	color: 'white',
+  	textAlign:'center'
+  },
+  button: {
+  	height: 45,
+  	flexDirection: 'row',
+  	backgroundColor: 'white',
+  	borderColor: 'white',
+  	borderWidth: 1,
+  	borderRadius: 8,
+  	marginBottom: 10,
+  	marginTop: 10,
+  	alignSelf: 'stretch',
+  	justifyContent: 'center'
+  },
+  buttonText: {
+  	fontSize: 18,
+  	color: '#111',
+  	alignSelf: 'center'
+  }
+});
+
+class Main extends React.Component{
+	constructor(props) {
+		super(props)
+		this.state = {
+			username: '',
+			isLoading: '',
+			error: ''
+		}
+	}
+
+	handleChange(e) {
+		this.setState({
+			username: e.nativeEvent.text
+		});
+	}
+
+	handleSubmit(e) {
+		this.setState({
+			isLoading: true
+		})
+		console.log('SUBMIT', this.state.username)
+	}
+
+	componentDidMount() {
+		console.log('Component mounted')
+	}
+	render() {
+		return (
+			<View style={styles.mainContainer}>
+			  <Text style={styles.title}>Search for a Github User</Text>
+			  <TextInput 
+			  	style={styles.searchInput}
+			  	value={this.state.username}
+			  	onChange={this.handleChange.bind(this)}
+			  />
+			  <TouchableHighlight
+			  	style={styles.button} 
+			  	onPress={this.handleSubmit.bind(this)}
+			  	underlayColor="white">
+			  	<Text style={styles.buttonText}>SEARCH</Text>
+			  </TouchableHighlight>
+			</View>
+		)
+	}
+}
+
+module.exports = Main;
---
+++ index.ios.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} from 'react-native';
+import Main from './App/Components/Main';
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  }
+})
+
+export default class safehippoRN extends Component {
+  render() {
+    return (
+      <NavigatorIOS 
+        initialRoute={{
+          component: Main,
+          title: 'Safehippo',
+        }}
+        style={styles.container}
+      />
+    );
+  }
+}
+
+AppRegistry.registerComponent('safehippoRN', () => safehippoRN);
