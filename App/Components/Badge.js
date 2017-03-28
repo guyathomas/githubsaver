b87a7d542429eda2d7248020d33f9be621a737a0
Guy <guythomas721@gmail.com>
2017-03-28 18:47:10 +1100

    Add badge to profile view

---
+++ App/Components/Badge.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Image
+} from 'react-native';
+
+const styles = StyleSheet.create({
+	image: {
+		height: 125,
+		width: 125,
+		borderRadius: 65,
+		marginTop: 10,
+		alignSelf: 'center'
+	},
+	container: {
+		backgroundColor: '#48BBEC',
+		paddingBottom: 10
+	},
+	name: {
+		alignSelf: 'center',
+		fontSize: 21,
+		marginTop: 10,
+		marginBottom: 5,
+		color: 'white'
+	},
+	handle: {
+		alignSelf: 'center',
+		fontSize: 16,
+		color: 'white'
+	}
+})
+
+class Badge extends React.Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Image style={styles.image} source={{uri: this.props.userinfo.avatar_url}} />
+				<Text style={styles.name} >{this.props.userinfo.name}</Text>
+				<Text style={styles.handle}>{this.props.userinfo.login}</Text>
+			</View>
+		)
+	}
+
+}
+
+Badge.propTypes = {
+	userInfo: React.PropTypes.object.isRequire
+};
+
+module.exports = Badge;
--- App/Components/Profile.js
+++ App/Components/Profile.js
@@ -4,22 +4,66 @@ import {
   Text,
   View,
   TouchableHighlight,
-  Image
+  Image,
+  ScrollView
 } from 'react-native';
+import Badge from './Badge'
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	buttonText: {
+		fontSize: 18,
+		color: 'white',
+		alignSelf: 'center'
+	},
+	rowContainer: {
+		padding: 10
+	},
+	rowTitle: {
+		color: '#48BBEC'
+	},
+	rowContent: {
+		fontSize: 19
+	}
+})
 
 class Profile extends React.Component{
 	constructor(props){
 		super(props)
 		this.state = {
-			
+
 		}
 	}
 
+	getRowTitle(item) {
+		const spacedWord = item.replace('_',' ');
+		return spacedWord[0] ? spacedWord[0].toUpperCase() + spacedWord.slice(1) : spacedWord[0]
+	}
+
 	render() {
+		const userinfo = this.props.userinfo;
+		const topicArr = ['company', 'location', 'followers', 'following', 'email', 'bio', 'public_repos']
+			debugger;
+		const list = topicArr.map((item, index) => {
+			if (!userinfo[item]) {
+				return (<View key={index} />);
+			} else {
+				return (
+					<View key={index}>
+						<View style={styles.rowContainer}>
+							<Text style={styles.rowTitle}>{this.getRowTitle.bind(this, item)}</Text>
+							<Text style={styles.rowContent}>{userinfo[item]} </Text>
+						</View>
+					</View>
+				)
+			}
+		})
 		return (
-			<View>
-				<Text>Profile</Text>
-			</View>
+			<ScrollView style={styles.container}>
+				<Badge userinfo={this.props.userinfo}/>
+			</ScrollView>
 		)
 	}
 }
